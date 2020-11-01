5c9b33ba3786052e38662d3159665386220010ca
aliasghar211 <aliasghar.aamir10@gmail.com>
2020-11-01 18:14:27 +0500

    Ali-Push

---
+++ android/app/src/main/kotlin/com/example/psych_help/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.psych_help
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      resizeToAvoidBottomPadding: false,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+              child: Stack(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+                child: Text('Hello',
+                    style:
+                        TextStyle(fontSize: 80.0, fontWeight: FontWeight.bold)),
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(15.0, 175.0, 0.0, 0.0),
+                child: Text('There',
+                    style:
+                        TextStyle(fontSize: 80.0, fontWeight: FontWeight.bold)),
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(220.0, 175.0, 0.0, 0.0),
+                child: Text('.',
+                    style: TextStyle(
+                        fontSize: 80.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green)),
+              )
+            ],
+          )),
+          Container(
+              padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+              child: Column(children: <Widget>[
+                TextFormField(
+                  cursorColor: Colors.green,
+                  cursorWidth: 2.0,
+                  decoration: InputDecoration(
+                      labelText: 'Email',
+                      labelStyle: TextStyle(
+                          fontSize: 30.0,
+                          color: Colors.grey,
+                          fontWeight: FontWeight.bold),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.green),
+                      )),
+                )
+              ])),
+          Container(
+              padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+              child: Column(children: <Widget>[
+                TextFormField(
+                  cursorColor: Colors.green,
+                  cursorWidth: 2.0,
+                  decoration: InputDecoration(
+                      labelText: 'Password',
+                      labelStyle: TextStyle(
+                          fontSize: 30.0,
+                          color: Colors.grey,
+                          fontWeight: FontWeight.bold),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.green),
+                      )),
+                )
+              ])),
+          SizedBox(height: 30.0),
+          Center(
+              child: Container(
+            height: 40.0,
+            width: 300.0,
+            child: Material(
+              borderRadius: BorderRadius.circular(20.0),
+              shadowColor: Colors.greenAccent,
+              elevation: 7.0,
+              color: Colors.green,
+              child: GestureDetector(
+                  onTap: () {},
+                  child: Center(
+                    child: Text(
+                      'LOGIN',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )),
+            ),
+          )),
+          SizedBox(height: 20.0),
+          Row(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(width: 50.0),
+                Text("Don't have an account?",
+                    style: TextStyle(color: Colors.grey)),
+                InkWell(
+                    onTap: () {},
+                    child: Text('Create a new account.',
+                        style: TextStyle(
+                            color: Colors.green,
+                            fontWeight: FontWeight.bold,
+                            decoration: TextDecoration.underline)))
+              ]),
+        ],
+      ),
+    );
+  }
+}
