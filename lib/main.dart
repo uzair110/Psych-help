feeda0869feabb1f4032452a4cd40a0c55d66302
Arham Saeed <saeedarham100@gmail.com>
2020-11-21 16:57:13 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -160,3 +160,83 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+class MyHomePage1 extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState1 extends State<MyHomePage1> {
+  TextEditingController username = new TextEditingController();
+  TextEditingController pwd = new TextEditingController();
+  Type rsp;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      resizeToAvoidBottomPadding: false,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+              child: Stack(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+                child: Text('Hello',
+                    style:
+                        TextStyle(fontSize: 80.0, fontWeight: FontWeight.bold)),
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(15.0, 175.0, 0.0, 0.0),
+                child: Text('There',
+                    style:
+                        TextStyle(fontSize: 80.0, fontWeight: FontWeight.bold)),
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(220.0, 175.0, 0.0, 0.0),
+                child: Text('.',
+                    style: TextStyle(
+                        fontSize: 80.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green)),
+              )
+            ],
+          )),
+          SizedBox(height: 30.0),
+          Center(
+              child: Container(
+            height: 40.0,
+            width: 300.0,
+            child: Material(
+              borderRadius: BorderRadius.circular(20.0),
+              shadowColor: Colors.greenAccent,
+              elevation: 7.0,
+              color: Colors.green,
+              child: GestureDetector(
+                  onTap: () async {
+                    print('User');
+                    result =
+                        await App_services.valEmployee(username.text, pwd.text);
+                    print(result);
+                    print(username.text);
+                    // valEmployee(username.text, pwd.text);
+                    print(pwd.text);
+                  },
+                  child: Center(
+                    child: Text(
+                      'Logout',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )),
+            ),
+          )),
+          SizedBox(height: 20.0),
+        ],
+      ),
+    );
+  }
+}
